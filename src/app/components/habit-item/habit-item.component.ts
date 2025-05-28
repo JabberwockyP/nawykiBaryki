dd38740ea2186fa7649641ed368e1fb28417b654
ATI2021 <franek.potwora@gmail.com>
2025-05-28 13:10:55 +0200

    dodanie komponentów

---
+++ src/app/components/habit-item/habit-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-item',
+  imports: [],
+  templateUrl: './habit-item.component.html',
+  styleUrl: './habit-item.component.css'
+})
+export class HabitItemComponent {
+
+}
