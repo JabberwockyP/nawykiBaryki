d0389f6b8f3741a1352842c86cf37587b115cd9d
ATI2021 <franek.potwora@gmail.com>
2025-06-04 13:10:51 +0200

    dodawanie nawykow

--- src/app/components/habit-list/habit-list.component.ts
+++ src/app/components/habit-list/habit-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HabitItemComponent } from '../habit-item/habit-item.component';
+import { HabitFormComponent } from '../habit-form/habit-form.component';
 
 @Component({
   selector: 'app-habit-list',
   standalone: true,
-  imports: [CommonModule, HabitItemComponent],
+  imports: [CommonModule, HabitItemComponent, HabitFormComponent],
   templateUrl: './habit-list.component.html'
 })
 export class HabitListComponent {
